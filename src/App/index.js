cf7c6b12f7f8251f0d16326f2e9f694e977f2849
Francesco Fortunati <fortunati.francesco89@gmail.com>
2019-04-12 23:14:26 +0200

    Set basic layout and style

---
+++ src/App/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+// Style
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: '',
+      spelling: '',
+    };
+
+    this.onNumberChange = this.onNumberChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.input.focus();
+  }
+
+  onNumberChange(e) {
+    this.setState({
+      number: e.target.value,
+      spelling: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="title">
+          <h1>NumberSpeller</h1>
+          <h6>Type a number (up to 2 quadrilion)</h6>
+        </header>
+
+        <input
+          className="input-number"
+          ref={node => this.input = node}
+          type="text"
+          value={this.state.number}
+          onChange={this.onNumberChange}
+        />
+
+        {this.state.spelling && <p>{this.state.spelling}</p>}
+      </div>
+    );
+  }
+}
+
+export default App;
