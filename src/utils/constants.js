4d07ca997c360124d4188f49a637704411a19525
Francesco Fortunati <fortunati.francesco89@gmail.com>
2019-04-13 16:16:57 +0200

    Create constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+export const strings = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+};
+
+export const tensStrings = {
+  2: 'twenty',
+  3: 'thirty',
+  4: 'fourty',
+  5: 'fifty',
+  6: 'sixty',
+  7: 'seventy',
+  8: 'eighty',
+  9: 'ninety',
+};
+
+export const thousandsStrings = {
+  1: 'thousand',
+  2: 'million',
+  3: 'billion',
+  4: 'trillion',
+  5: 'quadrillion',
+};
